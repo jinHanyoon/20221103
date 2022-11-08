5b3c3162979b41edbc4799d1ba759a2f515c5d41
jinHanyoon <104432921+jinHanyoon@users.noreply.github.com>
2022-11-08 17:54:10 +0900

    Add files via upload

---
+++ 연습용3/js/sub2.js
@@ -0,0 +1,12 @@
+
+
+function printName(){
+let UserNum = document.getElementById('sub2_Num').value;
+console.log(UserNum,'input 벨류값')
+let result =document.getElementById('result')
+result.innerHTML=UserNum*(1100000)+'원'
+
+
+
+}
+    
